7858c7e3463818c3e5696f9180a21872e768e288
devovi2024 <arfanhosenovi@gmail.com>
2026-04-23 21:48:16 +0600

    client

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://citypark-server.vercel.app/api",
+  withCredentials: true,
 });
 
 // ATTACH TOKEN AUTOMATICALLY
